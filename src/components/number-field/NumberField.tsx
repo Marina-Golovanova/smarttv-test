abc74434c8b71b612026d4efceb2b3c622f189b5
Marina <gma18@list.ru>
2021-08-09 17:40:58 +0300

    feat: add validation o number

--- src/components/number-field/NumberField.tsx
+++ src/components/number-field/NumberField.tsx
@@ -3,6 +3,7 @@ import { PhoneNumberMask } from "../phone-number-mask/PhoneNumberMask";
 import { NumberButtons } from "../number-buttons/NumberButtons";
 import { Checkbox } from "../checkbox/Checkbox";
 import { Button } from "../button/Button";
+import { api } from "../../api";
 
 import "./number-field.scss";
 
@@ -13,26 +14,48 @@ type NumberFieldProps = {
 export const NumberField: React.FC<NumberFieldProps> = (props) => {
   const [phoneNumber, setPhoneNumber] = React.useState<number[]>([]);
   const [checked, setChecked] = React.useState(false);
+  const [isIncorrectNumber, setIsIncorrectNumber] = React.useState<true | null>(
+    null
+  );
+
   const onChecked = () => {
     setChecked(!checked);
   };
 
   const onNumber = (n: number) => {
-    setPhoneNumber(phoneNumber.concat(n));
+    if (phoneNumber.length < 10) {
+      console.log(phoneNumber.length);
+      setPhoneNumber(phoneNumber.concat(n));
+    }
   };
 
   const onDelete = () => {
     setPhoneNumber(phoneNumber.slice(0, phoneNumber.length - 1));
   };
 
+  const onSubmit = async () => {
+    const res = await api.checkNumber(`7${phoneNumber.join("")}`);
+    if (res.valid) {
+      props.onSubmit();
+      setIsIncorrectNumber(null);
+    } else {
+      setIsIncorrectNumber(true);
+    }
+  };
+
   React.useEffect(() => {
+    let phoneNumberLength = 0;
     const handleKeyDown = (e: KeyboardEvent) => {
       if (isFinite(+e.key)) {
-        setPhoneNumber((phoneNumber) => phoneNumber.concat(+e.key));
+        if (phoneNumberLength < 10) {
+          setPhoneNumber((phoneNumber) => phoneNumber.concat(+e.key));
+          phoneNumberLength += 1;
+        }
       } else if (e.keyCode === 8) {
         setPhoneNumber((phoneNumber) =>
           phoneNumber.slice(0, phoneNumber.length - 1)
         );
+        phoneNumberLength -= 1;
       }
     };
 
@@ -58,9 +81,12 @@ export const NumberField: React.FC<NumberFieldProps> = (props) => {
         checked={checked}
         onChecked={onChecked}
       />
+      {isIncorrectNumber && (
+        <div className="number-field__error">неверно введен номер</div>
+      )}
       <Button
         text="подтвердить номер"
-        onClick={props.onSubmit}
+        onClick={onSubmit}
         stretch={true}
         disable={!(phoneNumber.length === 10) || !checked}
       />
